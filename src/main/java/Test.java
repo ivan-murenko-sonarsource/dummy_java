f0bfe947895148c01f2b63371d5c8412f9e3dc31
Ivan Murenko <ivan.murenko@sonarsource.com>
2022-11-30 14:27:32 +0100

    third commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,7 +3,7 @@ import java.util.stream.Stream;
 public class Test {
 
     public static void main(String[] args) {
-        Stream.of("Hello", " ", "world", "!")
+        Stream.of("Hello", " to the whole ", "world", "!")
                 .toList()
                 .forEach(System.out::print);
     }
