7797d4e5c694b23fb2632a7c3196d291392d2221
Ivan Murenko <ivan.murenko@sonarsource.com>
2022-11-30 14:30:35 +0100

    bug commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,9 +3,7 @@ import java.util.stream.Stream;
 public class Test {
 
     public static void main(String[] args) {
-        Stream.of("Hello", " to the whole ", "world", "!")
-                .toList()
-                .forEach(System.out::print);
+        int a = "a";
     }
 
 }
